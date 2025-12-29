d2987fd0595e3cb14dd3f15e4d69feebd82f19fc
woos <ddakgi00@naver.com>
2025-12-29 16:33:40 +0900

    echo 프로그램 개선

--- echo.py
+++ echo.py
@@ -1,3 +1,8 @@
-# Python 3: 입력한 문장을 그대로 출력
-s = input("문장을 입력하세요 : ")
-print(s)
+# Python 3: 문장을 반복 입력/출력, !quit 입력 시 종료
+
+while True:
+    s = input("문장을 입력하세요(!quit을 입력 시 종료) : ")
+    if s == "!quit":
+        print("프로그램을 종료합니다.")
+        break
+    print(s)
